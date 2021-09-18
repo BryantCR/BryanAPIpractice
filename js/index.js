09638c1fa4a7cef3ca3c5bd6eaa99f1fd95ba058
BryantCR <bryancvargas20@gmail.com>
2021-09-18 15:18:00 -0600

    ok

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function grabInfo( event ){
+    event.preventDefault();
+
+    let searchWeather = event.target.navsearch.value;
+}
+
+let weatherForm = document.querySelector('#weatherForm');
+weatherForm.addEventListener('submit', grabInfo);
